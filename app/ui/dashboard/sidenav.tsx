19c1803b03654fee20f3fef7629a005c84624d40
Aizaysi <aizaysi.success@gmail.com>
2024-08-06 09:56:10 -0700

    chapter 15_signout

--- app/ui/dashboard/sidenav.tsx
+++ app/ui/dashboard/sidenav.tsx
@@ -19,10 +19,10 @@ export default function SideNav() {
         <NavLinks />
         <div className="hidden h-auto w-full grow rounded-md bg-gray-50 md:block"></div>
         <form
-        action={async () => {
-          'use server';
-          await signOut();
-        }}
+          action={async () => {
+            'use server';
+            await signOut();
+          }}
         >
           <button className="flex h-[48px] w-full grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3">
             <PowerIcon className="w-6" />
